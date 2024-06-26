9966a9b50eac2676bface7f81f6f7fcc24de8fdd
goldenboy070S <sr080hz@gmail.com>
2024-06-26 11:09:09 +0500

    hihi commit

--- Desktop/project/dars5_uyvazifa.py
+++ Desktop/project/dars5_uyvazifa.py
@@ -1,16 +1,19 @@
 import psycopg2
 from decimal import Decimal
+from collections import namedtuple
+from dataclasses import dataclass
 
 
 class ExamDB:
     def __init__(self, database, user, password, host='localhost', port='5432'):
         self.conn = psycopg2.connect(database=database,
-                                user=user,
-                                password=password,
-                                host=host,
-                                port=port)
+                                     user=user,
+                                     password=password,
+                                     host=host,
+                                     port=port)
 
         self.cursor = self.conn.cursor()
+
     def projectDB(self):
         name = 'Samsung A32'
         price = 12.45869
@@ -18,6 +21,7 @@ class ExamDB:
         self.cursor.execute("INSERT INTO product (price, _name) VALUES (%s, %s);", priname)
         self.conn.commit()
         self.conn.rollback()
+
     def decimalkod(self, x, y):
         s = Decimal(str(x))
         b = Decimal(str(y))
@@ -31,3 +35,31 @@ class ExamDB:
 exe = ExamDB('exam', 'postgres', 'sardor2007a')
 # print(exe.decimalkod(float(input('birinchi sonni kiriting')), float(input('ikkinchi sonni kiriting'))))
 print(exe.projectDB())
+
+Mashina = namedtuple('Mashina', ['model', 'color', 'year', 'price'])
+
+
+class Car(Mashina):
+    def __new__(cls, model, color, year, price):
+        self = super(Car, cls).__new__(cls, model, color, year, price)
+        return self
+
+    def __str__(self):
+        if isinstance(self.model, str) and isinstance(self.color, str) and isinstance(self.year, int) and isinstance(self.price, int):
+            return (f"model: {self.model}\n"
+                f"rangi: {self.color}\n"
+                f"yili:  {self.year}\n"
+                f"narxi: {self.price}")
+        else:
+            raise TypeError('atributlarning tipi notogri')
+
+car = Car('bmw', 'qizil', 2027, 123455)
+print(car)
+
+
+@dataclass(frozen=True)
+class Car2:
+    model: str
+    color: str
+    year: int
+    price: int
